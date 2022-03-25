5b25c29403a5ee0221584abd4897ea2836daabdf
Tyranzx <70720366+Lewysan@users.noreply.github.com>
2022-03-25 15:10:52 -0700

    Create Example.sh

---
+++ Example.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Colors
+green="\e[0;32m\033[1m"
+closecolor="\033[0m\e[0m"
+clear='\033[0m'
+red="\e[0;31m\033[1m"
+purple="\e[0;35m\033[1m"
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+gray="\e[0;37m\033[1m"
+blue="\e[0;34m\033[1m"
+yellow="\e[0;33m\033[1m"
