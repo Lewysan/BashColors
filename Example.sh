b1c6ab12d5278722af6d2879c2c3c7556eead89c
Tyranzx <70720366+Lewysan@users.noreply.github.com>
2022-03-31 06:42:25 -0700

    Update Example.sh

--- Example.sh
+++ Example.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#Colors
+# Colors
 green="\e[0;32m\033[1m"
-closecolor="\033[0m\e[0m"
+white='\033[0;37m' 
+black='\033[0;30m'
 clear='\033[0m'
 red="\e[0;31m\033[1m"
 purple="\e[0;35m\033[1m"
@@ -11,5 +12,25 @@ cyan='\033[0;36m'
 gray="\e[0;37m\033[1m"
 blue="\e[0;34m\033[1m"
 yellow="\e[0;33m\033[1m"
+
+# Bold
+BBlack='\033[1;30m'
+BRed='\033[1;31m'
+BGreen='\033[1;32m'
+BYellow='\033[1;33m'
+BBlue='\033[1;34m'
+BPurple='\033[1;35m'
+BCyan='\033[1;36m'
+BWhite='\033[1;37m'
+
+# Underline
+UBlack='\033[4;30m'
+URed='\033[4;31m'
+UGreen='\033[4;32m'
+UYellow='\033[4;33m'
+UBlue='\033[4;34m'
+UPurple='\033[4;35m'
+UCyan='\033[4;36m'
+UWhite='\033[4;37m'
 
 echo -e "${gray} ${yellow}This a ${magenta}message, ${green}testing ${cyan}colors."
