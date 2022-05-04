a2c95b16b32602d39e63c580fc2d020373c4c22f
Tyranzx <70720366+Lewysan@users.noreply.github.com>
2022-05-04 15:10:03 +0100

    Update Example.sh

--- Example.sh
+++ Example.sh
@@ -13,16 +13,6 @@ gray="\e[0;37m\033[1m"
 blue="\e[0;34m\033[1m"
 yellow="\e[0;33m\033[1m"
 
-# Bold
-BBlack='\033[1;30m'
-BRed='\033[1;31m'
-BGreen='\033[1;32m'
-BYellow='\033[1;33m'
-BBlue='\033[1;34m'
-BPurple='\033[1;35m'
-BCyan='\033[1;36m'
-BWhite='\033[1;37m'
-
 # Underline
 UBlack='\033[4;30m'
 URed='\033[4;31m'
@@ -33,4 +23,3 @@ UPurple='\033[4;35m'
 UCyan='\033[4;36m'
 UWhite='\033[4;37m'
 
-echo -e "${gray} ${yellow}This a ${magenta}message, ${green}testing ${cyan}colors."
